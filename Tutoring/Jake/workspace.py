8a331689796d3fd1f52ee3c728692ea3e6ed4f75
Joe Davis <joedbns@gmail.com>
2022-03-21 16:20:00 -0400

    Add workspace fil;e for Jake

---
+++ Tutoring/Jake/workspace.py
@@ -0,0 +1,31 @@
+# txt = "Hello, welcome to my world. welcome"
+
+# search_find = txt.find("wel", 10, 35)
+# search_index = txt.index("wel")
+
+# print(search_find)
+# print(search_index)
+
+
+
+
+string_list = ['this', 'is', 'An', 'array', 'Of', 'is', 'an', 'strings', 'stinky']
+
+
+
+# # print(string_list[14])
+
+# print(string_list.index('array'))
+
+# print('is' in string_list)
+
+# print(len(string_list))
+
+
+
+
+# for item in string_list:
+#     if item.lower() == 'an':
+#         string_list.remove(item)
+
+# print(string_list)
