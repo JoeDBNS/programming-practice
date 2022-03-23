b47a9f1a04010b56d212ba432fe664abbab1c65e
Joe Davis <joedbns@gmail.com>
2022-03-22 21:32:24 -0400

    Add initial content for if statements

---
+++ python/4 - if statements/if.py
@@ -0,0 +1,67 @@
+
+test_string = "Hello there."
+
+test_number = ""
+
+true_bool = True
+
+# if (true_bool):
+#     print('TRUE')
+# else:
+#     print('else')
+
+
+
+
+
+# if (true_bool == True):
+#     print('TRUE')
+
+
+
+# if (true_bool == NULL):
+#     print('TRUE')
+# else:
+#     print('else')
+
+
+
+
+# if (true_bool == True):
+#     print('TRUE')
+# elif (true_bool == 'NULL'):
+#     print('NULL')
+# else:
+#     print('else')
+
+
+
+# if (test_string == 'test string'):
+#     print('TRUE')
+# elif (test_string == 'Hello there.'):
+#     print('NULL')
+# else:
+#     print('else')
+
+
+
+# if (test_string[1] == 'e'):
+#     print('TRUE')
+# else:
+#     print('else')
+
+
+
+
+
+# test = 'LL'
+
+# match len(test):
+#     case 1:
+#         print('True')
+
+#     case 2:
+#         print('False')
+
+#     case 3:
+#         print('NULL')
