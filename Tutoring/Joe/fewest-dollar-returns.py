afb2b34686aa90d7c9dbf2f86e88459b8ee6b4cb
Joe Davis <Joedbns@gmail.com>
2022-04-20 17:48:50 -0400

    Transform challenge answer into function

--- Tutoring/Joe/fewest-dollar-returns.py
+++ Tutoring/Joe/fewest-dollar-returns.py
@@ -4,24 +4,31 @@
 # using the fewest number of individual dollar bills.
 
 
-working_total = 127
 
-bill_types = [100, 50, 20, 10, 5, 1]
+def ReturnFewestBillAmounts(working_total):
+    bill_types = [100, 50, 20, 10, 5, 1]
 
-used_bills = {
-    1: 0,
-    5: 0,
-    10: 0,
-    20: 0,
-    50: 0,
-    100: 0
-}
+    used_bills = {
+        1: 0,
+        5: 0,
+        10: 0,
+        20: 0,
+        50: 0,
+        100: 0
+    }
 
-while working_total > 0:
-    if working_total >= bill_types[0]:
-        working_total -= bill_types[0]
-        used_bills[bill_types[0]] += 1
-    else:
-        del bill_types[0]
+    while working_total > 0:
+        if working_total >= bill_types[0]:
+            working_total -= bill_types[0]
+            used_bills[bill_types[0]] += 1
+        else:
+            del bill_types[0]
+    
+    return used_bills
 
-print(used_bills)
+
+
+print( ReturnFewestBillAmounts(127) )
+print( ReturnFewestBillAmounts(43) )
+print( ReturnFewestBillAmounts(999) )
+print( ReturnFewestBillAmounts(2) )
