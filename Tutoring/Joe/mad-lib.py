e66eb84a25c444b92a09c4cb95b40c690eb7563d
Joe Davis <Joedbns@gmail.com>
2022-04-20 18:31:49 -0400

    Build initial mad lib challenge

---
+++ Tutoring/Joe/mad-lib.py
@@ -0,0 +1,11 @@
+
+adj_1 = input("Enter an adjective: ")
+noun_1 = input("Enter a noun: ")
+name_1 = input("Enter a male name: ")
+verb_ing_1 = input("Enter a verb ending in 'ing': ")
+adj_2 = input("Enter an adjective: ")
+
+
+mad_lib_sentence = 'My ' + adj_1 + ' ' + noun_1 + ' had caught the attention of my neighbor ' + name_1 + ' while he was ' + verb_ing_1 + '. ' + name_1 + ' said that my ' + noun_1 + ' was ' + adj_2 + '.'
+
+print('\n\n' + mad_lib_sentence)
