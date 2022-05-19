3b0419b4e30f6c1a8aef6b3eaf81d60401fec016
Joe Davis <Joedbns@gmail.com>
2022-05-19 11:23:20 -0400

    Complete the hangman challenge

--- Tutoring/Joe/hangman.py
+++ Tutoring/Joe/hangman.py
@@ -57,7 +57,7 @@ ____|____  /-\\
     """
 ]
 
-secret_word = "fire pit"
+secret_word = ""
 guessed_letters = []
 
 user_guess_max = 6
@@ -71,7 +71,7 @@ def CollectUserGuess():
 
     while IsUserGuessValid(user_letter) == False:
         user_letter = input("Enter Letter Choice: ").replace(" ", "").lower()
-    
+
     guessed_letters.append(user_letter)
     return user_letter
 
@@ -85,21 +85,50 @@ def IsUserGuessValid(user_letter):
     elif user_letter in guessed_letters:
         print("\nThat letter has already been guessed.")
         return False
-    
+
     else:
         return True
 
 
 
+def BuildSecretWordDisplay():
+    secret_word_display = secret_word
+
+    for letter in secret_word_display:
+        if letter != " ":
+            if letter not in guessed_letters:
+                secret_word_display = secret_word_display.replace(letter, "_")
+
+    secret_word_display = " ".join(list(secret_word_display)).upper()
+
+    return secret_word_display
+
+
+
+
+secret_word = input("\nEnter the secret word: ").lower()
+
+print("\n\n\n\n\n\n\n" + hang_image_progression[user_guess_count])
+print(BuildSecretWordDisplay())
+print("Remaining Guesses:", user_guess_max - user_guess_count)
+
 while user_guess_count < user_guess_max:
     user_guess = CollectUserGuess()
+    BuildSecretWordDisplay()
 
-    if user_guess in secret_word:
-        print("hit")
-    else:
+    if user_guess not in secret_word:
         user_guess_count += 1
 
+    if BuildSecretWordDisplay().count("_") == 0:
+        break
+
     print("\n\n\n" + hang_image_progression[user_guess_count])
-    print("_ _ _ _ _")
+    print(BuildSecretWordDisplay())
     print("Remaining Guesses:", user_guess_max - user_guess_count)
     print("Guessed Letters:", str(guessed_letters).replace("\'", "").upper())
+
+if user_guess_count < user_guess_max:
+    print("\n\n\nThe word was: " + BuildSecretWordDisplay())
+    print("YOU'RE A WINNER!")
+else:
+    print("You've lost. Try again later.")
